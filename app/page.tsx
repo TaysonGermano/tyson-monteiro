077ad6a679b589fcf50d86aa9721e80f7bed1d0a
Tyson Monteiro <tyson@virtualdesigns.co.za>
2023-09-19 09:51:21 +0200

    added more info

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,15 @@ export default function Home() {
   return (
     <div className="pt-5">
       <div className="flex flex-row py-7 justify-between items-center">
-        <div className="hero-content">
+        <div className="hero-content w-[600px]">
           <p className="mt-6 text-sm text-slate-500 dark:text-slate-400 mb-2">
-            Welcome to my space
+            Tyson Monteiro - Full Stack JavaScript Developer
           </p>
           <h1 className="text-[1.75rem] font-extrabold leading-9 text-slate-900 dark:text-white md:text-4xl">
-            Hey I&apos;m Tyson <br />a Full-stack <span>Javascript</span>{" "}
-            developer
+            Hey there! I&apos;m Tyson Monteiro, a Full Stack JavaScript
+            developer with a laser focus on React.js and its vibrant ecosystem.
           </h1>
+          <p></p>
         </div>
         <div></div>
       </div>
