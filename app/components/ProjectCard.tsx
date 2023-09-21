c6beb140a041386024e9c5bc152470ce06cdbce1
Tyson Monteiro <tyson@virtualdesigns.co.za>
2023-09-21 17:00:04 +0200

    layout

---
+++ app/components/ProjectCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ProjectCard() {
+  return (
+    <div className="min-w-[390px] h-[300px] shadow-md bg-indigo-500 rounded-md "></div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+import ProjectCard from "./components/ProjectCard";
 
 export default function Home() {
   return (
@@ -13,9 +13,30 @@ export default function Home() {
             Hey there! I&apos;m Tyson Monteiro, a Full Stack JavaScript
             developer with a laser focus on React.js and its vibrant ecosystem.
           </h1>
-          <p></p>
         </div>
         <div></div>
+      </div>
+      <div className="mt-5 ">
+        <h2 className="text-2xl font-bold">Projects</h2>
+        <p className="text-md text-slate-500 dark:text-slate-400 mt-2 ">
+          Below is a list of project I was involved
+        </p>
+        <div className="mt-4 flex flex-row justify-between gap-4">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
+      </div>
+      <div className="mt-5 py-[80px] ">
+        <h2 className="text-2xl font-bold">My Stack</h2>
+        <p className="text-md text-slate-500 dark:text-slate-400 mt-2 ">
+          Tools and languages I mostly work with
+        </p>
+        <div className="mt-4 flex flex-row justify-between gap-4">
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+        </div>
       </div>
     </div>
   );
