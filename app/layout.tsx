8e2bd4e30e02c62c4e4d992b9229e2cd1b683029
TaysonGermano <taysongermano@gmail.com>
2024-04-08 02:33:35 +0200

    feat: exported footer into its own component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "../components/Navbar";
 import { FaLinkedin, FaEnvelope } from "react-icons/fa";
 import Link from "next/link";
 import ScroolToTop from "../components/ScroolToTop";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,23 +32,7 @@ export default function RootLayout({
             <Navbar />
           </header>
           <main className="mx-auto max-w-7xl px-4">{children}</main>
-          <footer className="mx-auto max-w-7xl px-4 py-5 mt-5 border-t dark:border-[#e2e8f00d]">
-            <p className="text-sm text-slate-300 text-center">
-              &copy; Tyson Monteiro {new Date().getFullYear()} &#183; built with
-              ♥
-            </p>
-            <div className="flex justify-center gap-3 mt-3">
-              <Link
-                href="https://www.linkedin.com/in/tyson-monteiro-59227a158/"
-                target="_blank"
-              >
-                <FaLinkedin size={20} color="#0b50b8" />
-              </Link>
-              <Link href="mailto:taysongermano@gmail.com" target="_blank">
-                <FaEnvelope size={20} color="#cccccc" />
-              </Link>
-            </div>
-          </footer>
+          <Footer />
           <ScroolToTop />
         </body>
       </html>
---
+++ components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import { FaLinkedin, FaEnvelope } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="mx-auto max-w-7xl px-4 py-5 mt-5 border-t dark:border-[#e2e8f00d]">
+      <p className="text-sm text-slate-300 text-center">
+        &copy; Tyson Monteiro {new Date().getFullYear()} &#183; built with ♥
+      </p>
+      <div className="flex justify-center gap-3 mt-3">
+        <Link
+          href="https://www.linkedin.com/in/tyson-monteiro-59227a158/"
+          target="_blank"
+        >
+          <FaLinkedin size={20} color="#0b50b8" />
+        </Link>
+        <Link href="mailto:taysongermano@gmail.com" target="_blank">
+          <FaEnvelope size={20} color="#cccccc" />
+        </Link>
+      </div>
+    </footer>
+  );
+}
