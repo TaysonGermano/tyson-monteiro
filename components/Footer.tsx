7e3a2326205319ee9f546b1b7fb6c6e7d83a8093
TaysonGermano <taysongermano@gmail.com>
2025-10-07 18:09:16 +0200

    failed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { FaLinkedin, FaEnvelope } from "react-icons/fa";
+// import { FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export default function Footer() {
   return (
